cee230f644691d8b9b1372886c34961d7b827155
Calvin Liang <calvin@nextdoor.com>
2018-11-23 10:47:56 -0800

    Basic layout

---
+++ Food Tracker/Food Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by calvin on 11/22/18.
+//  Copyright © 2018 calvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
